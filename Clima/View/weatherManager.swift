3c9a5850aeface9ad75b600e302c0eb30d963744
Mostafa Salama <105205855+MostaSalama@users.noreply.github.com>
2023-06-11 15:12:06 +0300

    added alamofire instead of native way to request data from API

--- Clima/View/weatherManager.swift
+++ Clima/View/weatherManager.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import CoreLocation
+import Alamofire
 
 protocol WeatherManagerDelegate {
     func didUpdateWeather(_ weatherManager:WeatherManager,weather:WeatherModel)
@@ -31,49 +32,56 @@ struct WeatherManager {
     }
     
     func performRequest(with urlString:String) {
-        //1 creat a url
-        if let url  = URL(string: urlString) {
-            //2 creat url session
-            let session = URLSession(configuration: .default)
-            //3 give the session a task
-            
-            let task  = session.dataTask(with: url) { data, response, error in
-                if error != nil {
-                    print(error!)
-                    self.delegate?.didFailWithError(error: error!)
-                    return
-                }
-                if let safeData = data {
-                    if let weather = self.parseJSON(safeData) {
-                        self.delegate?.didUpdateWeather(self, weather: weather)
-                    }
-                }
-            }
-            
-            //4 start the task
-            task.resume()
+        
+        let request =  AF.request(urlString)
+        request.responseDecodable(of:WeatherData.self) { response in
+            guard let response = response.value else {return}
+            let weather =  WeatherModel(conditionId: response.weather[0].id, cityName: response.name, temprature: response.main.temp)
+            self.delegate?.didUpdateWeather(self, weather: weather)
         }
+        //1 creat a url
+//        if let url  = URL(string: urlString) {
+//            //2 creat url session
+//            let session = URLSession(configuration: .default)
+//            //3 give the session a task
+//
+//            let task  = session.dataTask(with: url) { data, response, error in
+//                if error != nil {
+//                    print(error!)
+//                    self.delegate?.didFailWithError(error: error!)
+//                    return
+//                }
+//                if let safeData = data {
+//                    if let weather = self.parseJSON(safeData) {
+//                        self.delegate?.didUpdateWeather(self, weather: weather)
+//                    }
+//                }
+//            }
+//
+//            //4 start the task
+//            task.resume()
+//        }
     }
     
-    func parseJSON(_ weatherData:Data) -> WeatherModel? {
-        let decoder = JSONDecoder()
-        do {
-            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.main.temp)
-            
-            let id = decodedData.weather[0].id
-            let temp = decodedData.main.temp
-            let name = decodedData.name
-            
-            let weather = WeatherModel(conditionId: id, cityName: name, temprature: temp)
-            print(weather.conditionName)
-            print(weather.tempString)
-            return weather
-        } catch {
-            delegate?.didFailWithError(error: error)
-            return nil
-        }
-    }
+//    func parseJSON(_ weatherData:Data) -> WeatherModel? {
+//        let decoder = JSONDecoder()
+//        do {
+//            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+//            print(decodedData.main.temp)
+//            
+//            let id = decodedData.weather[0].id
+//            let temp = decodedData.main.temp
+//            let name = decodedData.name
+//            
+//            let weather = WeatherModel(conditionId: id, cityName: name, temprature: temp)
+//            print(weather.conditionName)
+//            print(weather.tempString)
+//            return weather
+//        } catch {
+//            delegate?.didFailWithError(error: error)
+//            return nil
+//        }
+//    }
     
     
     
